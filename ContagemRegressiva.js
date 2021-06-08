481b06320bf2e111e8329ef87fcd6f83a755ce99
Italo Costa <costa.italo.ti@gmail.com>
2021-06-08 19:28:14 -0300

    Imprime Contagem Regressiva

---
+++ ContagemRegressiva.js
@@ -0,0 +1,13 @@
+function solucao(numero) {
+
+    while (numero != -1) {
+        console.log(numero);
+        numero--;
+
+        if (numero === -1) {
+            console.log("KABUM")
+
+        }
+    }
+
+}
